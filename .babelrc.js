5d1018a0f04050c39e3adbaf493ed8adb0a82517
Kalle Hansson <kh.hansson@gmail.com>
2019-05-26 14:21:23 +0200

    Updated babelrc and some scripts

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,10 @@
 module.exports = {
 	presets: ['@babel/preset-env'],
+	plugins: [
+		'@babel/plugin-proposal-optional-chaining',
+		['@babel/plugin-proposal-decorators', { legacy: true }],
+		['@babel/plugin-proposal-class-properties', { loose: true }],
+	],
 	env: {
 		development: {},
 		production: {
